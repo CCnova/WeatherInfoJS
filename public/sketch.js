be7d9f2c117ee653eb2ad093c0a49a51eaf922c3
CNova <caiocnova1@gmail.com>
2020-07-27 15:08:42 -0300

    Show air quality information on screen

--- public/sketch.js
+++ public/sketch.js
@@ -6,18 +6,27 @@ function setup() {
     navigator.geolocation.getCurrentPosition( async position => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-      document.getElementById('latitude').textContent = `Latitude: ${lat}`;
-      document.getElementById('longitude').textContent = `Longitude: ${long}`;
-
+      
       const apiUrl = `/weather/${long},${lat}`;
       const response = await fetch(apiUrl);
       const jsonFormatReponse = await response.json();
       console.log(jsonFormatReponse);
-
+      
       const precipitation = document.getElementById('precipitation');
       const rating = document.getElementById('precipRating');
+      
+      const airQ = jsonFormatReponse.airQuality.results[0].measurements[0];
+      
+      
       precipitation.textContent = jsonFormatReponse.weather.precipitation_type.value;
       rating.textContent = jsonFormatReponse.weather.precipitation.value;
+
+      document.getElementById('latitude').textContent = `Latitude: ${lat}`;
+      document.getElementById('longitude').textContent = `Longitude: ${long}`;
+      document.getElementById('aqParameter').textContent = airQ.parameter;
+      document.getElementById('aqValue').textContent = airQ.value;
+      document.getElementById('aqUnits').textContent = airQ.unit;
+      document.getElementById('aqDate').textContent = airQ.lastUpdated;
     });
   } else {
     console.log('Geolocation is not available!\nCan not get user location');
