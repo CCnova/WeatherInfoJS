ab9c5adec767c72f6a25160d03e230898594fb59
CNova <caiocnova1@gmail.com>
2020-07-27 08:22:43 -0300

    Get user latitude and longitude

---
+++ public/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  noCanvas();
+  let lat, long;
+
+  const btn = document.getElementById('submit');
+  btn.addEventListener('click', async evResult => {
+    const locationData = { lat, long };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(locationData)
+    }
+
+    const response = await fetch('/api', options);
+    const jsonResponse = await response.json();
+
+    console.log(jsonResponse);
+  });
+
+  if ('geolocation' in navigator) {
+    console.log('Geolocation is available!\nGetting user location...');
+    navigator.geolocation.getCurrentPosition( async position => {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      console.log(lat, long);
+      document.getElementById('latitude').textContent = `Latitude: ${lat}`;
+      document.getElementById('longitude').textContent = `Longitude: ${long}`;
+    });
+  } else {
+    console.log('Geolocation is not available!\nCan not get user location');
+  }
+}
